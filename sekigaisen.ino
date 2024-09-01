6ec0329c5c94143b081ef58ac6070c18fc9de015
aoi <aoi256jp@Gmail.com>
2024-09-01 12:40:48 +0900

    Create sekigaisen.ino

---
+++ sekigaisen.ino
@@ -0,0 +1,29 @@
+double sekigaisen_value;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(2,INPUT); 
+  pinMode(4,OUTPUT);
+  delay(500);//初期化待ち（一応）
+
+}
+
+void loop() {
+   
+  sekigaisen_value = analogRead(A12);//ADC12のアナログ変換値を取得
+
+  //Serial.println(sekigaisen_value); //アナログ変換値を出力(デバッグ用）
+
+  if(sekigaisen_value >= 3500.0){
+
+    digitalWrite(4, HIGH);  
+
+  }
+  else{
+
+    digitalWrite(4, LOW);
+
+  }
+}
